d57c5c67dd2f2c97207e09061a3d61b31560c8ce
ss92 <sumitss92@yahoo.com>
2017-09-09 18:13:39 +0530

    service content added

--- js/main.js
+++ js/main.js
@@ -16,36 +16,6 @@ $(function() {
     });
 });
 
-;(function($){
-    'use strict';
-    $.fn.smoothScroll = function(setting) {
-        var _default = {
-                duration: 1000,
-                easing: 'swing',
-                offset: 0
-            },
-            _setting = $.extend(_default, setting),
-            _handler = function() {
-                var dest = 0,
-                    target = this.hash,
-                    $target = $(target);
-
-                $(this).on('click', function(e){
-                    e.preventDefault();
-                    if ($target.offset().top > ($(document).height() - $(window).height())) {
-                        dest = $(document).height() - $(window).height();
-                    } else {
-                        dest = $target.offset().top - _setting.offset;
-                    }
-                    $('html, body').animate({
-                        scrollTop: dest
-                    }, _setting.duration, _setting.easing);
-                });
-            };
-        return this.each(_handler);
-    };
-})(jQuery);
-
 
 jQuery(document).ready(function($){
     // browser window scroll (in pixels) after which the "back to top" link is shown
@@ -75,3 +45,33 @@ jQuery(document).ready(function($){
     });
 
 });
+
+(function($){
+    'use strict';
+    $.fn.smoothScroll = function(setting) {
+        var _default = {
+                duration: 1000,
+                easing: 'swing',
+                offset: 0
+            },
+            _setting = $.extend(_default, setting),
+            _handler = function() {
+                var dest = 0,
+                    target = this.hash,
+                    $target = $(target);
+
+                $(this).on('click', function(e){
+                    e.preventDefault();
+                    if ($target.offset().top > ($(document).height() - $(window).height())) {
+                        dest = $(document).height() - $(window).height();
+                    } else {
+                        dest = $target.offset().top - _setting.offset;
+                    }
+                    $('html, body').animate({
+                        scrollTop: dest
+                    }, _setting.duration, _setting.easing);
+                });
+            };
+        return this.each(_handler);
+    };
+})(jQuery);
